b27b48e1ddcd2b15bd52471829cb47fa4590ae06
Björn Kahlert <mail@bkahlert.com>
2020-08-13 00:52:29 +0200

    Integrate ext4 driver installation

---
+++ resources/prepare.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# shellcheck source=shared.sh
+BASEDIR=$(dirname "$0") && . "$BASEDIR"/shared.sh
+if command -v fuse-ext2 &>/dev/null; then macosPrepared=$(_h " alreay prepared"); fi
+
+_title "Reflector Image Preparation"
+
+_p "This tools aims to facilitate setting up your reflector as much as possible."
+_p "Currently only the current lite version of Raspberry Pi OS is offered."
+_option 1 "Raspberry Pi OS (32-bit) Lite" "Suitable for Raspberry Pi B+/2/3/4 & Zero Rev.1.3/W/WH"
+_option 9 "Mac OS X$macosPrepared" "Installs the required packages OSX Fuse / Fuse for macOS and Fuse Ext2 in order to mount the images to be prepared."
+_option ESC "Quit"
+_p
+
+_prompt "So?" "Press ESC or any other key to quit."
+
+_p
+case $REPLY in
+1)
+    _p "$(_grey Preparing image...)"
+    _p
+    _run "prepare---image"
+    ;;
+9)
+    _p "$(_grey Preparing Mac OS X...)"
+    _p
+    _run "prepare---mac-os"
+    _p
+    _run "prepare"
+    ;;
+*)
+    _p "$(_red Quitting...)"
+    _p
+    ;;
+esac
