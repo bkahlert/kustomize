efd7d6b07c566395eeff08edccd0af672dc26dd1
Björn Kahlert <mail@bkahlert.com>
2020-08-20 15:03:52 +0200

    Kotlin draft

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,21 @@
-val kotlin_version: String by project
-val ktor_version: String by project
-val krossbow_version: String by project
-
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.3.72"
+    kotlin("jvm") version "1.4.0"
+    application
 }
-
-group = "com.bother-you.reflector"
+group = "com.imgcstmzr"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
-    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
     mavenCentral()
-    jcenter()
 }
-
 dependencies {
-    implementation(kotlin("stdlib-js"))
-    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
-
-    implementation("org.hildan.krossbow:krossbow-stomp-core-metadata:$krossbow_version")
-    implementation("org.hildan.krossbow:krossbow-stomp-core-js:$krossbow_version")
-
-//    implementation("io.ktor:ktor-client-websockets:$ktor_version")
-//    implementation("io.ktor:ktor-client-cio:$ktor_version")
-//    implementation("io.ktor:ktor-client-js:$ktor_version")
-    implementation(npm("inline-style-prefixer"))
-    implementation(npm("iframe"))
-
-    testImplementation(kotlin("test-js"))
+    testImplementation(kotlin("test-junit5"))
 }
-
-kotlin {
-    target {
-        browser {
-            distribution {
-                directory = File("$projectDir/output/")
-                webpackTask {}
-            }
-        }
-    }
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+application {
+    mainClassName = "MainKt"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,1 @@
-pluginManagement {
-    repositories {
-        maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
-        mavenCentral()
-        maven { setUrl("https://plugins.gradle.org/m2/") }
-    }
-}
-rootProject.name = "reflector"
+rootProject.name = "imgcstmzr"
